a9df5c52222b905e5bed4bb5c01a9caaaf3c4097
saadbenallal4 <saadbenallal4@gmail.com>
2026-02-21 15:00:30 +0100

    Remove node_modules from tracking

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+/* CHARGEMENT DES VARIABLES D’ENVIRONNEMENT
+   dotenv permet de charger les variables
+   définies dans le fichier .env
+   et de les rendre accessibles via process.env */
+require('dotenv').config();
+
 /* IMPORTATIONS DES DEPENDANCES */
 const express = require('express');
 const mongoose = require('mongoose');
@@ -16,8 +22,11 @@ app.use(express.json());
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', authRoutes);
 
-/* CONNEXION A MONGODB */
-mongoose.connect('mongodb://localhost:27017/monvieuxgrimoire')
+/* CONNEXION A MONGODB 
+L’URI de connexion est stockée dans le .env
+pour éviter d’exposer des informations sensibles
+dans le code */
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('Connexion à MongoDB réussie !');
     })
@@ -25,8 +34,11 @@ mongoose.connect('mongodb://localhost:27017/monvieuxgrimoire')
         console.error('Connexion à MongoDB échouée !', error);
     });
 
-/* DEFINITION DU PORT*/
-const PORT = 4000;
+/* DEFINITION DU PORT
+ Le port est récupéré depuis le fichier .env
+ Cela permet de modifier le port sans toucher
+ au code source */
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur le port ${PORT}`);
